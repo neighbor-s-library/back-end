998ae06943d96c73b3916c9639526eb9d5a1bdb6
kim-hyeji <hyeji8755@gmail.com>
2021-06-23 19:17:05 +0900

    [김혜지] RentalRestController - 빌린, 빌려준 목록 구분

--- book/HelloBook/src/main/java/study/spring/hellobook/controllers/RentalRestController.java
+++ book/HelloBook/src/main/java/study/spring/hellobook/controllers/RentalRestController.java
@@ -1,6 +1,7 @@
 package study.spring.hellobook.controllers;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,9 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 import study.spring.hellobook.helper.MailHelper;
 import study.spring.hellobook.helper.RegexHelper;
 import study.spring.hellobook.helper.WebHelper;
+import study.spring.hellobook.model.BookList;
 import study.spring.hellobook.model.Rental;
+import study.spring.hellobook.model.Users;
 import study.spring.hellobook.service.JwtService;
 import study.spring.hellobook.service.RentalService;
+import study.spring.hellobook.service.UsersService;
 
 @RestController
 public class RentalRestController {
@@ -39,6 +43,9 @@ public class RentalRestController {
 	/** Service 패턴 구현체 주입 */
 	@Autowired
 	RentalService rentalService;
+	
+	@Autowired
+	UsersService usersService;
 	
 	@Autowired
 	JwtService jwtService;
@@ -146,12 +153,16 @@ public class RentalRestController {
 		return webHelper.getJsonData(map);
 	}
 	
-	/** 대여 목록 조회 */
-	@RequestMapping(value = "/rental/{user_id}", method = RequestMethod.GET)
+	/** 대여 목록 조회 - Owner 빌려준 책 목록 조회 */
+	@RequestMapping(value = "/owner/{user_id}", method = RequestMethod.GET)
 	public Map<String, Object> rental(@PathVariable("user_id") int user_id,
 			@RequestHeader("Token") String token) {
 		
-		//jwt 유효성 검사
+		if (token == null) {
+			return webHelper.getJsonWarning("토큰이 없습니다.");
+		}
+
+		// jwt 유효성 검사
 		Map<String, Object> claimMap = null;
 		try {
 			claimMap = jwtService.verifyJWT(token);
@@ -159,39 +170,126 @@ public class RentalRestController {
 			e1.printStackTrace();
 		}
 
+		if (claimMap == null) {
+			return webHelper.getJsonWarning("토큰이 유효하지 않습니다.");
+		}
+
 
 		// 조회할 값들을 Beans에 담는다.
 		Rental input = new Rental();
 		input.setOwner_id(user_id);
-		input.setRenter_id(user_id);
 
 		// 저장된 결과를 조회하기 위한 객체
 		List<Rental> output_owner = null;
-		List<Rental> output_renter = null;
-		int ownerCounterBook = 0;
-		int renterCounterBook = 0;
+		
+		//유효한 사용자 번호인지 확인
+		Users user = new Users();
+		user.setId(user_id);
+		Users user_output = new Users();
+		
+		List<BookList> output = new ArrayList<BookList>();
 
 		try {
 				// 데이터 조회
+			user_output = usersService.getUserItem(user);
 			output_owner = rentalService.getOwnerList(input);
-			output_renter = rentalService.getRenterList(input);
-			ownerCounterBook = rentalService.getOwnersBookCount(input);
-			renterCounterBook = rentalService.getRentersBookCount(input);
 			
 			
 		} catch (Exception e) {
 			return webHelper.getJsonError(e.getLocalizedMessage());
 		}
+		
+		if(user_output == null) {
+			System.out.println("사용자 번호가 유효하지 않음");
+		}
+		
+		//저장된 결과를 조회할 최종 객체
+		for (int i = 0; i < output_owner.size(); i++) {
+			Rental item = output_owner.get(i);
+			BookList booklist = new BookList();
+			booklist.setTitle(item.getTitle());
+			booklist.setImg(item.getImg());
+			booklist.setCreated_at(item.getCreated_at());
+			output.add(booklist);
+		}
+		
+		
 
 		/** 3) 결과를 확인하기 위한 페이지 연동 */
 		// 저장 결과를 확인하기 위해 데이터 저장 시 생성된 PK값을 상세 페이지로 전달해야 한다.
 		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("ownerList", output_owner);
-		map.put("renterList", output_renter);
-		map.put("ownerCounterBook", ownerCounterBook); // 빌려준 책 개수 조회
-		map.put("renterCounterBook", renterCounterBook); //빌린책 개수 조회
-		map.putAll(claimMap);
+		map.put("output", output);
 		return webHelper.getJsonData(map);
 	}
+	
+	/** 대여 목록 조회 - Renter 빌린 책 목록 조회 */
+	@RequestMapping(value = "/renter/{user_id}", method = RequestMethod.GET)
+	public Map<String, Object> rental2(@PathVariable("user_id") int user_id,
+			@RequestHeader("Token") String token) {
+		
+		if (token == null) {
+			return webHelper.getJsonWarning("토큰이 없습니다.");
+		}
+
+		// jwt 유효성 검사
+		Map<String, Object> claimMap = null;
+		try {
+			claimMap = jwtService.verifyJWT(token);
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+
+		if (claimMap == null) {
+			return webHelper.getJsonWarning("토큰이 유효하지 않습니다.");
+		}
+
+
+		// 조회할 값들을 Beans에 담는다.
+		Rental input = new Rental();
+		input.setRenter_id(user_id);
+
+		// 저장된 결과를 조회하기 위한 객체
+		List<Rental> output_renter = null;
+		
+		//유효한 사용자 번호인지 확인
+		Users user = new Users();
+		user.setId(user_id);
+		Users user_output = new Users();
+		
+		List<BookList> output = new ArrayList<BookList>();
+
+		try {
+				// 데이터 조회
+			user_output = usersService.getUserItem(user);
+			output_renter = rentalService.getRenterList(input);
+			
+			
+		} catch (Exception e) {
+			return webHelper.getJsonError(e.getLocalizedMessage());
+		}
+		
+		if(user_output == null) {
+			System.out.println("사용자 번호가 유효하지 않음");
+		}
+		
+		//저장된 결과를 조회할 최종 객체
+		for (int i = 0; i < output_renter.size(); i++) {
+			Rental item = output_renter.get(i);
+			BookList booklist = new BookList();
+			booklist.setTitle(item.getTitle());
+			booklist.setImg(item.getImg());
+			booklist.setCreated_at(item.getCreated_at());
+			output.add(booklist);
+		}
+		
+		
+
+		/** 3) 결과를 확인하기 위한 페이지 연동 */
+		// 저장 결과를 확인하기 위해 데이터 저장 시 생성된 PK값을 상세 페이지로 전달해야 한다.
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("output", output);
+		return webHelper.getJsonData(map);
+	}
+	
 	
 }
---
+++ book/HelloBook/src/main/java/study/spring/hellobook/model/BookList.java
@@ -0,0 +1,10 @@
+package study.spring.hellobook.model;
+
+import lombok.Data;
+
+@Data
+public class BookList {
+	private String title;
+	private String img;
+	private String created_at;
+}
